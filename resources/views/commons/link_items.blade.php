900269d5df7d607f2cffbdcd7c0d5fec8f769bc9
miyu.fujii <miyu-fujii@dac.co.jp>
2023-06-01 04:22:25 +0000

    z-indexの調整

--- resources/views/commons/link_items.blade.php
+++ resources/views/commons/link_items.blade.php
@@ -8,7 +8,7 @@
                 <path fill-rule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clip-rule="evenodd" />
             </svg>
         </a>
-        <ul class="p-2 bg-base-100 shadow">
+        <ul class="p-2 bg-base-100 shadow z-50">
             <li><a class="link link-hover text-gray-700" href="{{ route('users.show', Auth::user()->id) }}">My profile</a></li>
             <li><a class="link link-hover text-gray-700" href="{{ route('users.favorites', Auth::user()->id) }}">Favorites</a></li>
             <li><a class="link link-hover text-red-500" href="#" onclick="event.preventDefault();this.closest('form').submit();">Logout</a></li>
