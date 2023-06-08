3b6c17be9238d9118d2d80dd35c7d1c6ec055bd0
miyu.fujii <miyu-fujii@dac.co.jp>
2023-06-08 02:57:21 +0000

    UIの微調整

--- resources/views/user_follow/follow_button.blade.php
+++ resources/views/user_follow/follow_button.blade.php
@@ -4,14 +4,25 @@
         <form method="POST" action="{{ route('user.unfollow', $user->id) }}">
             @csrf
             @method('DELETE')
+            
             <button type="submit" class="btn btn-error btn-block normal-case" 
-                onclick="return confirm('id = {{ $user->id }} のフォローを外します。よろしいですか？')">Unfollow</button>
+                onclick="return confirm('id = {{ $user->id }} のフォローを外します。よろしいですか？')">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+                  <path d="M10.375 2.25a4.125 4.125 0 100 8.25 4.125 4.125 0 000-8.25zM10.375 12a7.125 7.125 0 00-7.124 7.247.75.75 0 00.363.63 13.067 13.067 0 006.761 1.873c2.472 0 4.786-.684 6.76-1.873a.75.75 0 00.364-.63l.001-.12v-.002A7.125 7.125 0 0010.375 12zM16 9.75a.75.75 0 000 1.5h6a.75.75 0 000-1.5h-6z" />
+                </svg>
+                &nbsp;Unfollow
+            </button>
         </form>
     @else
         {{-- フォローボタンのフォーム --}}
         <form method="POST" action="{{ route('user.follow', $user->id) }}">
             @csrf
-            <button type="submit" class="btn btn-primary btn-block normal-case">Follow</button>
+            <button type="submit" class="btn btn-info btn-block normal-case">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+                  <path d="M6.25 6.375a4.125 4.125 0 118.25 0 4.125 4.125 0 01-8.25 0zM3.25 19.125a7.125 7.125 0 0114.25 0v.003l-.001.119a.75.75 0 01-.363.63 13.067 13.067 0 01-6.761 1.873c-2.472 0-4.786-.684-6.76-1.873a.75.75 0 01-.364-.63l-.001-.122zM19.75 7.5a.75.75 0 00-1.5 0v2.25H16a.75.75 0 000 1.5h2.25v2.25a.75.75 0 001.5 0v-2.25H22a.75.75 0 000-1.5h-2.25V7.5z" />
+                </svg>
+                &nbsp;Follow
+            </button>
         </form>
     @endif
 @endif
