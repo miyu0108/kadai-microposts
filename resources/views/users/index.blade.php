da69424355e0f8fc390ac6e1d0ad641e3b9e7de3
miyu.fujii <miyu-fujii@dac.co.jp>
2023-06-13 02:06:47 +0000

    検索機能UIの改善

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -4,14 +4,18 @@
     
     <form method="GET" action="{{ route('users.search') }}">
         @csrf
-        <input type="search" placeholder="ユーザー名を入力" name="search" value="@if (isset($search)) {{ $search }} @endif">
-        <div>
-            <button class='btn btn-sm' type="submit">検索</button>
-            <button>
-                <a href="{{ route('users.index') }}" class="btn btn-sm bg-white text-black hover:bg-white hover:text-black">
-                    クリア
-                </a>
-            </button>
+        
+        <div class="flex my-4">
+            <div class="form-control w-1/2">
+                <input class="input input-bordered" type="text" placeholder="ユーザー名を入力" name="search" value="@if (isset($search)) {{ $search }} @endif">
+            </div>
+            <div>
+                <button class='btn' type="submit">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                      <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+                    </svg>
+                </button>
+            </div>
         </div>
     </form>
     {{-- ユーザ一覧 --}}
